d1d12418ca2a3e6bb7b95bed9743b253722f3141
Fernando Lozano <flozano@redhat.com>
2018-12-18 18:11:28 -0200

    Bumped vert.x version, no change. Also tried to add truststore configs to keycloakjson and httpOptions, none worked :-(

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # vertx-hello
-Hello, World application for Vert.x, Red Hat OpenShift Application Runtimes, and RH-SSO (Keycloak)
+Hello, World application for Vert.x and Red Hat OpenShift Application Runtimes
 
 To compile and run locally, you need the Red Hat Maven repositories. A sample Maven settings.xml file is provided in the conf folder.
 
@@ -9,11 +9,11 @@ $ mvn -Popenshift fabric8:deploy
 
 To build and deploy to openshift using a source workflow, log in to openshift, create a project, and run:
 
-$ oc new-app --name vertx-sso redhat-openjdk18-openshift:1.3~https://github.com/flozanorht/vertx-sso.git
+$ oc new-app --name vertx-hello redhat-openjdk18-openshift:1.3~https://github.com/flozanorht/vertx-hello.git
 
 (you need to expose the application after the build finishes)
 
-The settings for the RH-SSO realm are hard-coded in MainVerticle.java
+The resource URL is '/api/hello/AnyName'. Prepend with either 'localhost:8080' or the OpenShift route host name.
 
 WIP: DO NOT CLONE YET!
 
--- src/main/java/com/redhat/training/flozano/MainVerticle.java
+++ src/main/java/com/redhat/training/flozano/MainVerticle.java
@@ -3,6 +3,8 @@ package com.redhat.training.flozano;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.net.JksOptions;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.StaticHandler;
@@ -29,9 +31,14 @@ public class MainVerticle extends AbstractVerticle {
 
         LOG.info("Starting Hello app...");
 
+            //.put("truststore", "/home/flozano/sso.truststore")
+            //.put("truststore-password", "password")
+            //.put("disable-trust-manager", true)
+            //.put("allow-any-hostname", true)
+
         JsonObject keycloakJson = new JsonObject()
             .put("realm", "vertx-hello")
-            .put("auth-server-url", "http://localhost:8180/auth")
+            .put("auth-server-url", "https://localhost:8543/auth")
             .put("ssl-required", "external")
             .put("resource", "website")
             .put("public-client", true)
@@ -41,18 +48,36 @@ public class MainVerticle extends AbstractVerticle {
         LOG.info("RH-SSO realm: " + keycloakJson.getString("realm"));
         LOG.info("RH-SSO server: " + keycloakJson.getString("auth-server-url"));
         
+       HttpClientOptions httpOptions = new HttpClientOptions()
+           .setVerifyHost(false)
+           .setTrustAll(true)
+           //.setTrustStoreOptions(new JksOptions()
+               //.setPath("/home/flozano/sso.truststore")
+               //.setPassword("password"))
+       ;
+       
         Router router = Router.router(vertx);
 
-        OAuth2Auth oauth2 = KeycloakAuth.create(vertx, OAuth2FlowType.AUTH_CODE, keycloakJson);        
+        //OAuth2Auth oauth2 = KeycloakAuth.create(vertx, OAuth2FlowType.AUTH_CODE, keycloakJson);
+        OAuth2Auth oauth2 = KeycloakAuth.create(vertx, OAuth2FlowType.AUTH_CODE, keycloakJson, httpOptions);                
         OAuth2AuthHandler oauth2Handler = OAuth2AuthHandler.create(oauth2);
         oauth2Handler.setupCallback(router.get("/callback"));
         
         router.route("/protected/*").handler(oauth2Handler);
+        router.route("/privateapi/*").handler(oauth2Handler);
         
         router.route("/*").handler(StaticHandler.create("html")
             .setCachingEnabled(false)
         );
         
+        router.get("/publicapi/hello/:greeting")
+            .produces("application/text")
+            .handler(this::hello);
+
+        router.get("/privateapi/hello/:greeting")
+            .produces("application/text")
+            .handler(this::hello);
+
         vertx.createHttpServer()
             .requestHandler(router::accept)
             .listen(8080, result -> {
@@ -66,4 +91,22 @@ public class MainVerticle extends AbstractVerticle {
             });
     }
     
+    //XXX should use a confidential instead of a public realm?
+    
+    private void hello(RoutingContext rc) {
+    	String greeting = rc.request().getParam("greeting");
+        String host = rc.request().host();
+
+        String user = "anonymous";
+        if (rc.user() != null && rc.user().principal() != null) {
+            String accessToken = KeycloakHelper.rawAccessToken(rc.user().principal());
+            JsonObject token = KeycloakHelper.parseToken(accessToken);
+            user = token.getString("preferred_username");
+        }
+          
+        LOG.info("Got API request with greeting = '" + greeting + "' ...");
+        
+      	rc.response().end(greeting + " from " + user + "@" + host + "\n");
+    }
+
 }
